db8ce6008f043857113a5b4a39bcd6e3bbe49826
zy <zhouya2010@163.com>
2016-11-15 17:48:58 +0800

    add:viewpager

--- app/src/main/java/com/example/zy/lucautils/ui/fragmen/DailyFragment.java
+++ app/src/main/java/com/example/zy/lucautils/ui/fragmen/DailyFragment.java
@@ -1,16 +1,18 @@
 package com.example.zy.lucautils.ui.fragmen;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import com.example.zy.lucautils.R;
 import com.example.zy.lucautils.app.App;
 import com.example.zy.lucautils.base.SimpleFragment;
 import com.example.zy.lucautils.model.bean.DailyListBean;
+import com.example.zy.lucautils.ui.adapter.TopPagerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,12 +28,18 @@ import rx.schedulers.Schedulers;
  */
 
 public class DailyFragment extends SimpleFragment {
-    @BindView(R.id.swipe_refresh)
-    SwipeRefreshLayout swipeRefresh;
+//    @BindView(R.id.swipe_refresh)
+//    SwipeRefreshLayout swipeRefresh;
 
     String currentDate;
-//    DailyAdapter mAdapter;
-    List<DailyListBean.StoriesEntity> mList = new ArrayList<>();
+    //    DailyAdapter mAdapter;
+    List<DailyListBean.TopStoriesEntity> mList = new ArrayList<>();
+    @BindView(R.id.vp_top)
+    ViewPager vpTop;
+    @BindView(R.id.ll_point_container)
+    LinearLayout llPointContainer;
+    @BindView(R.id.swipe_refresh)
+    SwipeRefreshLayout swipeRefresh;
 
     @Override
     protected int getLayoutId() {
@@ -40,6 +48,9 @@ public class DailyFragment extends SimpleFragment {
 
     @Override
     protected void initEventAndData() {
+
+        final TopPagerAdapter adapter = new TopPagerAdapter(getContext(), mList);
+
         App.getAppComponent().retrofitHelper().fetchDailyListInfo()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -56,8 +67,17 @@ public class DailyFragment extends SimpleFragment {
 
                     @Override
                     public void onNext(DailyListBean dailyListBean) {
+                        mList.addAll(dailyListBean.getTop_stories());
+                        vpTop.setAdapter(adapter);
                     }
                 });
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
 }
